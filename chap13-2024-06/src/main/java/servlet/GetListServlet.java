54776c72738cfcaf9558049044c434f0f69fe6da
SeiichiN <billie175@gmail.com>
2025-01-16 14:15:10 +0900

    chap13-2024-06 modified.

--- chap13-2024-06/src/main/java/servlet/GetListServlet.java
+++ chap13-2024-06/src/main/java/servlet/GetListServlet.java
@@ -8,10 +8,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import model.Employee;
 import model.GetListLogic;
 
-<<<<<<<< HEAD:chap13-2024-06/src/main/java/servlet/GetListServlet.java
 @WebServlet("/getList")
 public class GetListServlet extends HttpServlet {
 	
@@ -22,14 +22,6 @@ public class GetListServlet extends HttpServlet {
 		List<Employee> empList = logic.execute();
 		request.setAttribute("empList", empList);
 		String url = "WEB-INF/jsp/find/empList.jsp";
-========
-@WebServlet(urlPatterns = {"/createInput"})
-public class InputServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "WEB-INF/jsp/input/input.jsp";
->>>>>>>> main:empManager_2tables/src/main/java/servlet/InputServlet.java
 		request.getRequestDispatcher(url).forward(request, response);
 	}
 
